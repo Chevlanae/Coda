ca4d4ac4909ee21f38484fe69cdd06ddc16c6bee
Chevlanae <leahvance6312@gmail.com>
2022-03-30 21:01:10 +0000

    Added Catalogs dynamic sync table

--- formulas.ts
+++
@@ -1,2 +0,0 @@
-import type * as coda from "@codahq/packs-sdk"
-
--- pack.ts
+++ pack.ts
@@ -1,9 +1,11 @@
 import * as coda from "@codahq/packs-sdk";
+
 import { CampaignSchema } from "./schemas";
+import { generateObjectSchemaProperties } from "./helpers";
 
 export const pack = coda.newPack();
 
-pack.setSystemAuthentication({
+pack.setUserAuthentication({
 	type: coda.AuthenticationType.CustomHeaderToken,
 	headerName: "Api-Key",
 	instructionsUrl: "https://support.iterable.com/hc/en-us/articles/360043464871-API-Keys-",
@@ -12,7 +14,7 @@ pack.setSystemAuthentication({
 pack.addNetworkDomain("api.iterable.com");
 
 pack.addSyncTable({
-	name: "AllCampaigns",
+	name: "Campaigns",
 	schema: CampaignSchema,
 	identityName: "Campaign",
 	formula: {
@@ -52,6 +54,84 @@ pack.addSyncTable({
 							!value.name.includes("campaign from Workflow") &&
 							!value.name.includes("*ARCHIVED*")
 					),
+			};
+		},
+	},
+});
+
+pack.addDynamicSyncTable({
+	name: "Catalogs",
+	description: "Dynamic table that displays a selected catalog from your Iterable project",
+	listDynamicUrls: async function (context) {
+		let response = await context.fetcher.fetch({
+			method: "GET",
+			url: "https://api.iterable.com/api/catalogs",
+		});
+
+		let catalogList: Array<any> = response.body.params.catalogNames;
+
+		return catalogList.map((entry) => {
+			return { display: entry.name, value: `https://api.iterable.com/api/catalogs/${entry.name}` };
+		});
+	},
+	getName: async function (context) {
+		let url: string = context.sync.dynamicUrl,
+			name = url.split("/").slice(-1)[0];
+		return name;
+	},
+	getDisplayUrl: async function (context) {
+		let url: string = context.sync.dynamicUrl,
+			name = url.split("/").slice(-1)[0];
+		return `https://app.iterable.com/catalogs/table/${name}`;
+	},
+	getSchema: async function (context) {
+		let response = await context.fetcher.fetch({
+				method: "GET",
+				url: context.sync.dynamicUrl + "/items",
+			}),
+			sampleData = response.body.params.catalogItemsWithProperties[0].value,
+			featured = [...Object.keys(sampleData), "dateModified"],
+			properties: coda.ObjectSchemaProperties = {
+				...generateObjectSchemaProperties(sampleData),
+				itemId: { type: coda.ValueType.String },
+				dateModified: { type: coda.ValueType.String },
+			};
+
+		let schema = coda.makeObjectSchema({
+			properties: properties,
+			id: "itemId",
+			primary: "itemId",
+			featured: featured,
+			identity: {
+				name: "Entry",
+				dynamicUrl: context.sync.dynamicUrl,
+			},
+		});
+
+		return coda.makeSchema({
+			type: coda.ValueType.Array,
+			items: schema,
+		});
+	},
+	formula: {
+		name: "SyncCatalog",
+		description: "Sync your Iterable project's Catalogs",
+		parameters: [],
+		execute: async function ([], context) {
+			let response = await context.fetcher.fetch({
+					method: "GET",
+					url: context.sync.dynamicUrl + "/items?pageSize=99999",
+				}),
+				items: Array<any> = response.body.params.catalogItemsWithProperties;
+
+			return {
+				result: items.map((entry) => {
+					return {
+						...entry.value,
+						itemId: entry.itemId,
+						dateModified: new Date(entry.lastModified).toString(),
+					};
+				}),
 			};
 		},
 	},
